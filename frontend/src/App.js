6f3ed5db41f5105206bb3f9abeded22230f1a50f
devansh19jain <devanshjain1910@gmail.com>
2024-08-09 15:11:46 +0530

    Home Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,20 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-
-import "./styles.css";
-import RequireAuth from "./Components/RequireAuth";
+import Home from "./Components/Home/Home";
+// import "./App.css";
+import './index.css'; 
+// import RequireAuth from "./Components/RequireAuth";
 const App = () => {
   return (
     <>
       <div className="app">
         
           <Routes>
-            <Route path='/' element={<Layout />}>
-              <Route path="login" element={<Login />}/>
+            <Route path='/' element={<Home />}>
+              {/* <Route path="login" element={<Login />}/> */}
               
               {/*Protected Routes*/ }
-              <Route element={<RequireAuth allowedRoles={[2020, 2021, 2022, 2023]}/>}>
+              {/* <Route element={<RequireAuth allowedRoles={[2020, 2021, 2022, 2023]}/>}>
               <Route path="diary" element={<Dashboard />} />
               <Route path="blogs" element={<AddTrip />} />
               <Route path="chat" element={<Books />} />
@@ -22,7 +23,7 @@ const App = () => {
               </Route>
               <Route element={<RequireAuth allowedRoles={[2020, 20021, 2022]}/>}>
                 <Route path="chat" element={<Update />} />
-              </Route>
+              </Route> */}
            
 
               {/* catch all*/}
